8fa7532dbfcdd5f8374098d6e5d942b387088bcb
Finn Price <fp19361@ms.wasatch.edu>
2025-02-26 15:33:12 -0700

    Revert "Tutorial Level that goes into Corbin Level"
    This reverts commit a7b565f42dc064108ceea6a5530ee8c1cbee5161.

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -9,8 +9,8 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private float horizontal;
-    [SerializeField] private float speed = 8f;
-    [SerializeField] private float jumpingPower = 16f;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
     private bool isFacingRight = true;
 
     private bool isWallSliding;
@@ -121,14 +121,6 @@ public class PlayerMovement : MonoBehaviour
     {
         return Physics2D.OverlapCircle(groundCheck.position, .75f, groundLayer);
     }
-
-    
-    // what if instead of william afton it was freaky william - finn
-   
-
-   
-
-    
 
     private bool IsWalled()
     {
