fd69f153ecbdb4ef47c4e33a4c4cf5dcafdc9e69
Corbin Cunningham (the great evil) <Corbin.Cunningham@live.com>
2025-02-26 13:31:52 -0700

    Dont pull from this 5:
    Disfruta la gloriosa suavemente mi amigo

--- Assets/_Scripts/LockedDoor.cs
+++ Assets/_Scripts/LockedDoor.cs
@@ -29,15 +29,7 @@ public class LockedDoor : MonoBehaviour
         {
             //If the GameObject's name matches the one you suggest, output this message in the console
             Debug.Log("Colliding with: " + _player);
-            
-        }
-
-        //Check for a match with the specific tag on any GameObject that collides with your GameObject
-        if (collision.gameObject.tag == "Friend")
-        {
-            //If the GameObject has the same tag as specified, output this message in the console
-            Debug.Log("Colliding with: " + _player);
-            
+            Destroy(gameObject);
         }
     }
     
