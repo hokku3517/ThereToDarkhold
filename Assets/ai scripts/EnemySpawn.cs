03020cd59bb647e2c7dc8f66be35b535882cc6c4
Corbin Cunningham (the great evil) <Corbin.Cunningham@live.com>
2025-03-20 13:35:24 -0600

    Finn grab they work-eduh

--- Assets/ai scripts/EnemySpawn.cs
+++ Assets/ai scripts/EnemySpawn.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
+    [SerializeField] private GameObject player;
     [SerializeField] private GameObject EnemyOffspring;
-    [SerializeField] private GameObject hi;
+    [SerializeField] private int EnemySpawnAmount;
+    
+    private Vector3 hi;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +20,30 @@ public class EnemySpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("I spawn forth");
-        
-        hi = Instantiate(EnemyOffspring, gameObject.transform);
-        
+        //hi = Instantiate(EnemyOffspring, gameObject.transform);
+       
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+		
+		//Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.name == "Player")
+        {
+            float x = gameObject.transform.position.x;
+            float y = gameObject.transform.position.y;
+            float z = gameObject.transform.position.z;
+
+            transform.localPosition = new Vector3(x,y,z);
+
+            
+            //hi = Instantiate(EnemyOffspring, gameObject.transform);
+            for (int i = 0; i < EnemySpawnAmount; i++){
+                GameObject.Instantiate(EnemyOffspring, transform.localPosition, Quaternion.identity);
+            }
+            Destroy(gameObject);
+            
+        }
         
     }
+
 }
