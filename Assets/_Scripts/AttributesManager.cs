8a9485a47626ce3707229a57a0f0a6e961a55f61
Corbin Cunningham (the great evil) <Corbin.Cunningham@live.com>
2025-04-10 14:32:20 -0600

    Camera cool and memes freaky minecraft movie ervil lebaron

--- Assets/_Scripts/AttributesManager.cs
+++ Assets/_Scripts/AttributesManager.cs
@@ -7,8 +7,12 @@ public class AttributesManager : MonoBehaviour
     
     public int autoAmount = 1;
     public int heavyWindAmount = 20;
-    public int health;
-    
+    public int health = 100;
+
+    [SerializeField] GameObject heart1;
+    [SerializeField] GameObject heart2;
+    [SerializeField] GameObject heart3;
+    [SerializeField] GameObject heart4;
 
     public void TakeDamage(int amount)
     {
@@ -39,6 +43,21 @@ public class AttributesManager : MonoBehaviour
    
     void Update()
     {
-        
+
+        if (health > 75 && health <= 100){
+            //4hearts do nothing
+        } else if (health > 50 && health <= 75){
+            //3 heartts delete heart 4
+            Destroy(heart4);
+        } else if (health > 25 && health <= 50){
+            //2 heartts delete heart 3
+            Destroy(heart3);
+        } else if (health > 1 && health <= 25){
+            //1 heartts delete heart 2
+            Destroy(heart2);
+        } else if (health < 1){
+            //0 hearts delete heart 1
+            Destroy(heart1);
+        }
     }
 }
