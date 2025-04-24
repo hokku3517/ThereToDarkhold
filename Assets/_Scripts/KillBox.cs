714aed1ec9ae94ccaf81f3546c4fa9cd4c312d17
Finn Price <fp19361@ms.wasatch.edu>
2025-04-24 14:45:01 -0600

    Freaky Spike does Freaky Damage
    Still need to refreakify 2nd tutorial

--- Assets/_Scripts/KillBox.cs
+++ Assets/_Scripts/KillBox.cs
@@ -2,14 +2,26 @@ using UnityEngine;
 
 public class Killbox : MonoBehaviour
 {
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Friend"))
+        if (other.CompareTag("Friend") && gameObject.name == "Kill Box")
         {
             AttributesManager attributesManager = other.GetComponent<AttributesManager>();
             if (attributesManager != null)
             {
                 attributesManager.health = 0;
+            }
+            else
+            {
+                Debug.LogWarning("AttributesManager not found on player!");
+            }
+        } else if (other.CompareTag("Friend") && gameObject.name == "Freaky Spike")
+        {
+            AttributesManager attributesManager = other.GetComponent<AttributesManager>();
+            if (attributesManager != null)
+            {
+                attributesManager.health -= 25;
             }
             else
             {
