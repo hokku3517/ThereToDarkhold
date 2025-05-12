93ee7ea531237fab23f64bb10a6ce79c0e908f3e
Corbin Cunningham (the great evil) <Corbin.Cunningham@live.com>
2025-05-12 13:33:23 -0600

    Gun shoot

---
+++ Assets/_Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ExampleCoroutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator ExampleCoroutine()
+        {
+            //Print the time of when the function is first called.
+            Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+            //yield on a new YieldInstruction that waits for 5 seconds.
+            yield return new WaitForSeconds(5);
+            Destroy(gameObject);
+
+            //After we have waited 5 seconds print the time again.
+            Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        }
+}
--- Assets/_Scripts/Gun.cs
+++ Assets/_Scripts/Gun.cs
@@ -6,25 +6,37 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     
-
+    
     [SerializeField] private GameObject SecondAmmendment;
     [SerializeField] private GameObject Player;
 
-    [SerializeField] private float mouseX;
-    [SerializeField] private float mouseY;
+    [SerializeField] private GameObject Bullet;
+    [SerializeField] private Rigidbody2D BulletRigidbody;
+
+    private float mouseX;
+    private float mouseY;
+
+    [SerializeField] private int bulletSpeed = 100;
+
+    float angle;
+
+    GameObject instance;
 
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        BulletRigidbody = Bullet.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RotationLogic();
+        ShootingLogic();
+    }
 
+    void RotationLogic(){
         Vector3 mousePos = Input.mousePosition;
 		mousePos.z = 5.23f;
 
@@ -32,7 +44,7 @@ public class Gun : MonoBehaviour
 		mousePos.x = mousePos.x - objectPos.x;
 		mousePos.y = mousePos.y - objectPos.y;
 
-		float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+		angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
         
 		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
@@ -44,5 +56,42 @@ public class Gun : MonoBehaviour
         } else {
             SecondAmmendment.transform.localScale = new Vector3(-1,1,1);
         }
+    }
+
+    void ShootingLogic(){
+        if (Input.GetMouseButtonDown(0)) // Left mouse button click
+        {
+            instance = Instantiate(Bullet, SecondAmmendment.transform.position, Quaternion.identity);
+            /* 
+            float xcomponent = Mathf.Cos(angle * Mathf.PI / 180) * bulletSpeed;
+            float ycomponent = Mathf.Sin(angle * Mathf.PI / 180) * bulletSpeed;
+            Vector3 asjfdn = new Vector3(ycomponent, 0, xcomponent);
+            */
+
+            Vector3 dir = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.right;
+            
+            instance.GetComponent<Rigidbody2D>().AddForce(dir * bulletSpeed);
+            
+            
+
+            /*
+            Debug.Log("amgonsu");
+            RaycastHit2D ray = Physics2D.Raycast(transform.position, Player.transform.position - transform.position); 
+            if (ray.collider != null)
+            {
+                // Raycast hit something!
+                Debug.DrawRay(transform.position, SecondAmmendment.transform.position - transform.position, Color.red);
+                Debug.Log("Hit");
+                // Handle the hit (e.g., damage the object, play a sound, etc.)
+            }
+            else
+            {
+                // Raycast didn't hit anything
+                Debug.DrawRay(transform.position, SecondAmmendment.transform.position - transform.position, Color.red);
+                Debug.Log("No hit");
+            }
+            */
+        }
+            
     }
 }
