02ac4ee1564d7f84bdf654974d48e1a6ff3ba4ad
Corbin Cunningham (the great evil) <Corbin.Cunningham@live.com>
2025-05-14 12:49:30 -0600

    Hi im the whip

--- Assets/_Scripts/Bullet.cs
+++ Assets/_Scripts/Bullet.cs
@@ -16,15 +16,25 @@ public class Bullet : MonoBehaviour
         
     }
     IEnumerator ExampleCoroutine()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+         
+    public void HandleCollision(Collision2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Enemy"))
         {
-            //Print the time of when the function is first called.
-            Debug.Log("Started Coroutine at timestamp : " + Time.time);
-
-            //yield on a new YieldInstruction that waits for 5 seconds.
-            yield return new WaitForSeconds(5);
-            Destroy(gameObject);
-
-            //After we have waited 5 seconds print the time again.
-            Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+            Destroy(collision.gameObject);
         }
+    }
+
 }
